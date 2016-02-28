eefbfab8ae127c0c9d56b7981b4d6b067d7ea24a
Zac B <bradyzp@gmail.com>
2016-02-28 20:24:54 +0100

    Rewrote DSC Resource Path Script

--- New-DSCVirtualMachine.ps1
+++ New-DSCVirtualMachine.ps1
@@ -7,7 +7,8 @@
     [string]$VMSwitch,    
     [string]$VMHost = "localhost",
     [string]$ISOPath,
-    [switch]$Running = $false
+    [switch]$Running = $false,
+    [int]$VMGeneration = 2
 )
 
 Configuration MakeVM {
@@ -18,12 +19,27 @@ Configuration MakeVM {
         [String]$VMPath,
         [Parameter(Mandatory)]
         [String]$VMName,
-        [String]$VMSwitch
+        [String]$VMSwitch,
+        [int]$Generation = 2
     )
     
     Import-DscResource -ModuleName xHyper-V
+    Import-DscResource -ModuleName PSDesiredStateConfiguration
 
     Node $HyperVNode {
+
+        File VHDDir {
+            DestinationPath = "$VMPath\Virtual Hard Disks"
+            Ensure = "Present"
+            Force = $true
+            Type = 'Directory'
+        }
+        File VMDir {
+            DestinationPath = "$VMPath\Virtual Machines"
+            Ensure = "Present"
+            Force = $true
+            Type = 'Directory'
+        }
 
         xVHD NewVHD {
             Ensure = "Present"
@@ -31,27 +47,31 @@ Configuration MakeVM {
             Path = "$VMPath\Virtual Hard Disks"
             Generation = "Vhdx"
             MaximumSizeBytes = 50GB
+            DependsOn = "[File]VHDDir"
         }
 
         xVMHyperV NewVM {
             Ensure = "Present"
             Name = $VMName
-            Path = $VMPath
-            Generation = 2
+            Path = "$VMPath\Virtual Machines"
+            Generation = $Generation
             StartupMemory = 2048MB
             State = "Off"
             ProcessorCount = 4
             SwitchName = $VMSwitch
             VhdPath = "$VMPath\Virtual Hard Disks\$VMName.vhdx"
-            DependsOn = "[xVHD]NewVHD"
+            DependsOn = @("[xVHD]NewVHD","[File]VMDir")
         }
     }
 }
 
 Remove-Item $PSScriptRoot/*.mof -Force
 
-MakeVM -HyperVNode $VMHost -VMPath $VMPath -VMName $VMName -VMSwitch $VMSwitch -outputpath $PSScriptRoot
-Start-DscConfiguration -Path $PSScriptRoot -Credential (import-clixml "$PSScriptRoot\Creds\administrator.clixml") -Wait -Force -verbose
+MakeVM -HyperVNode $VMHost -VMPath $VMPath -VMName $VMName -VMSwitch $VMSwitch -Generation $VMGeneration -outputpath $PSScriptRoot
+Start-DscConfiguration -Path $PSScriptRoot -Credential (Get-Credential -Message "DSC Config Credential") -Wait -Force -verbose
+
+#Clean up mof file
+Remove-Item $PSScriptRoot/*.mof -Force
 
 Remove-Module Hyper-V
 Import-Module Hyper-V -RequiredVersion '1.1'
